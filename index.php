176af12a882589021eadd9f6544b8afd34b3ae64
Szabo Marius <szabomarius@gmail.com>
2014-03-07 13:13:25 +0200

    Put in the html markup for the header
    also changed the custom.js because it had some useless functions

--- index.php
+++ index.php
@@ -16,6 +16,13 @@
 <!-- HEADER -->
 <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
 <header class="main_header">
+	<h1>Szabo Marius</h1>
+	<h2>Some things about the web</h2>
+	<ul>
+		<li><img src="#" alt="twitter">
+			<a href="#">Twitter</a>
+		</img></li>
+	</ul>
 </header> <!-- end of .main_header -->
 
 <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
@@ -35,7 +42,7 @@
 <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <!-- If google can't load jQuery, load it from different sources -->
-<script>window.jQuery || document.write('<script src="http://code.jquery.com/jquery-1.11.0.min.js">\x3C/script>')</script>-->
+<script>window.jQuery || document.write('<script src="http://code.jquery.com/jquery-1.11.0.min.js">\x3C/script>')</script>
 <script>window.jQuery || document.write('<script src="js/jQuery.js">\x3C/script>')</script>
 <!-- Website Javascript -->
 <script src="js/custom.js"></script>
