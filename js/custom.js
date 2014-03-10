74ed03a4a337d1b32b876465d726b3d375bcc570
Szabo Marius <szabomarius@gmail.com>
2014-03-10 11:49:18 +0200

    Small modifications to clean up the code

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,9 @@
-// Social Icon Text Variables
-var twitter_text = "Follow me on Twitter!";
-var facebook_text = "Like me on Facebook!";
-var googleplus_text = "Join me on GooglePlus!";
-var linkedin_text = "View my LinkedIn Profile!";
-var github_text = "Follow me on GitHub!";
-
 // Finds what link this is (based on a data-tip html attribute) and changes tooltip to reflect this
 function change_tooltip_content() {
 	var currentDatatype = $(this).attr("data-tip");
 	return currentDatatype;
 }
+//JqueryUI tooltip declaration
 $(".social_links li").tooltip({
 	content: change_tooltip_content(),
 	show: {
